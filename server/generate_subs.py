466d6627eecc1b96aaf64d584780f8e3e66274f7
Shiv-Desktop <shivsarthak34@gmail.com>
2023-03-15 23:06:00 +0530

    updated srt generation

--- server/generate_subs.py
+++ server/generate_subs.py
@@ -10,6 +10,7 @@ import os
 import hashlib
 from celery.signals import task_postrun
 from flask_socketio import SocketIO
+from whisper.utils import WriteSRT
 
 
 app = Flask(__name__)
@@ -17,18 +18,23 @@ CORS(app)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 75
 app.config['UPLOAD_EXTENSIONS'] = ['mp4', 'avi', 'mov']
 app.config['PROCESSING_MODE'] = ['fast', 'balanced', 'accurate']
-app.config['UPLOAD_FOLDER'] = os.environ.get('UPLOAD_FOLDER',os.path.abspath('./tmp/uploads/'))
-app.config['OUTPUT_FOLDER'] = os.environ.get('OUTPUT_FOLDER',os.path.abspath('./tmp/output/'))
-app.config['CELERY_BROKER_URL'] = os.environ.get('CELERY_BROKER_URL','redis://192.168.0.191:6379')
-app.config['CELERY_BACKEND_URL'] = os.environ.get('CELERY_BACKEND_URL','redis://192.168.0.191:6379')
+app.config['UPLOAD_FOLDER'] = os.environ.get(
+    'UPLOAD_FOLDER', os.path.abspath('./tmp/uploads/'))
+app.config['OUTPUT_FOLDER'] = os.environ.get(
+    'OUTPUT_FOLDER', os.path.abspath('./tmp/output/'))
+app.config['CELERY_BROKER_URL'] = os.environ.get(
+    'CELERY_BROKER_URL', 'redis://192.168.0.191:6379')
+app.config['CELERY_BACKEND_URL'] = os.environ.get(
+    'CELERY_BACKEND_URL', 'redis://192.168.0.191:6379')
 
-if os.environ.get('BUILD_MODE','DEV') == "PROD":
+if os.environ.get('BUILD_MODE', 'DEV') == "PROD":
     app.config['HASH_SALT'] = os.urandom(8).hex()
 else:
     app.config['HASH_SALT'] = b'testingsalt'.hex()
 
 socketio = SocketIO(app, cors_allowed_origins="*")
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_BACKEND_URL'])
+celery = Celery(
+    app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_BACKEND_URL'])
 celery.conf.update(app.config)
 celery.conf.update(result_extended=True)
 
@@ -36,48 +42,33 @@ celery.conf.update(result_extended=True)
 def hash_id(id: str):
     return hashlib.md5((id+app.config['HASH_SALT']).encode('utf-8')).hexdigest()[0:8]
 
+
 @celery.task(trail=True, bind=True)
 def generate_subtitles_task(self, video_name, model_size):
+    start = datetime.now()
     print(self.request.id)
     model = whisper.load_model(model_size)
     video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_name)
     audio = whisper.load_audio(video_path)
+    srt_file_path = os.path.join(
+        app.config['OUTPUT_FOLDER'], video_name.split('.')[0]+'.srt')
     try:
         transcript = model.transcribe(audio)
-        captions = []
-        for segment in transcript['segments']:
-            captions.append(
-                {'start': int(segment['start']*1000),
-                 'end': int(segment['end']*1000),
-                 'text': segment['text']
-                 }
-            )
-        subtitle_file = ''
-        for i, caption in enumerate(captions, start=1):
-            subtitle_file += str(i)+'\n'
-            subtitle_file += '{:02d}:{:02d}:{:02d},{:03d} --> {:02d}:{:02d}:{:02d},{:03d}\n'.format(
-                caption['start'] // 3600000,
-                (caption['start'] // 60000) % 60,
-                (caption['start'] // 1000) % 60,
-                caption['start'] % 1000,
-                caption['end'] // 3600000,
-                (caption['end'] // 60000) % 60,
-                (caption['end'] // 1000) % 60,
-                caption['end'] % 1000
-            )
-            subtitle_file += caption['text']+'\n\n'
-            srt_file_path = os.path.join(
-                app.config['OUTPUT_FOLDER'], video_name.split('.')[0]+'.srt')
-        with open(srt_file_path, 'w') as file:
-            file.write(subtitle_file)
+        srt_writer = WriteSRT('.')
+        with open(srt_file_path, 'w') as out:
+            srt_writer.write_result(transcript, out)
+        end = datetime.now()
+        timetaken = (end-start).seconds
 
-        return video_name.split('.')[0]+'.srt'
+        return f"Executed in: {timetaken} s"
     except Exception as e:
         abort(500, 'Error processing the video file: {}'.format(str(e)))
 
+
 @task_postrun.connect(weak=False)
 def task_postrun_handler(sender=None, task_id=None, **kwargs):
-    socketio.emit(task_id, {'msg':'ok'},namespace='/api/events')
+    socketio.emit(task_id, {'msg': 'ok'}, namespace='/api/events')
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['UPLOAD_EXTENSIONS']
@@ -85,7 +76,7 @@ def allowed_file(filename):
 
 @app.route('/api')
 def post_run():
-    return jsonify({'status':'Running'}),200
+    return jsonify({'status': 'Running'}), 200
 
 
 @app.route('/api/upload_video', methods=['POST'])
@@ -169,11 +160,9 @@ def download_file(tid: str):
     if (status.successful()):
         filename = os.path.join(app.config['OUTPUT_FOLDER'], status.result)
         if os.path.isfile(filename):
-            return send_file(filename,as_attachment=True), 200
+            return send_file(filename, as_attachment=True), 200
     return jsonify(
         {'state': "404",
             'error': f'File Not Found'
          }
     ), 404
-
-
